d48b7f71c5fce21b0d891b8753ff08f660e75095
N0tilT <timtompson3241@gmail.com>
2023-03-20 16:50:00 +0300

    IncidenceMatrix
    without creating with adjacencyList

--- AllAboutGraph/AllAboutGraph/MVC/Model/IncidenceMatrix.cs
+++ AllAboutGraph/AllAboutGraph/MVC/Model/IncidenceMatrix.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,116 @@ namespace AllAboutGraph.MVC.Model
 {
     internal class IncidenceMatrix
     {
+        #region Fields
+        private int[,] _matrix;
+        private int _numOfVertices;
+        private int _numOfEdges;
+        #endregion
+
+        #region Properties
+
+        public int[,] Matrix
+        {
+            get {return _matrix;}
+            set { _matrix = value;}
+        } 
+
+        public int CountVertices
+        {
+            get { return _matrix.GetLength(0); }
+        }
+
+        public int CountEdges
+        {
+            get { return _matrix.GetLength(1); }
+        }
+        #endregion
+
+        public IncidenceMatrix()
+        {
+            _matrix = new int[0, 0];
+            _numOfVertices = 0;
+            _numOfEdges= 0;
+        }
+
+        public IncidenceMatrix(int[,] inidenceMatrix, bool isIncident)
+        {
+            _matrix = inidenceMatrix;
+        }
+
+        public IncidenceMatrix(AdjacencyMatrix adjMatrix)
+        {
+            _matrix = FromAdjacencyMatrix(adjMatrix);
+        }
+
+        public IncidenceMatrix(AdjacencyList adjList)
+        {
+            _matrix = FromAdjacencyList(adjList);
+        }
+
+        private int[,] FromAdjacencyMatrix(AdjacencyMatrix adjMatrix)
+        {
+            int numOfVertices = adjMatrix.Matrix.GetLength(0);
+            int numOfEdges = 0;
+
+            for (int i = 0; i < numOfVertices; i++)
+            {
+                for (int j = 0; j < numOfVertices; j++)
+                {
+                    if (i < j)
+                    {
+                        if (adjMatrix.Matrix[i, j] > 0)
+                        {
+                            numOfEdges++;
+                        }
+                        if (adjMatrix.Matrix[j,i]>0 && adjMatrix.Matrix[j, i]!= adjMatrix.Matrix[i, j])
+                        {
+                            numOfEdges++;
+                        }
+                    }
+                }
+            }
+
+            int[,] incedenceMatrix = new int[numOfVertices, numOfEdges];
+            for (int i = 0; i < numOfVertices; i++)
+            {
+                for (int j = 0; j < numOfVertices; j++)
+                {
+                    if (i < j)
+                    {
+                        int edgeNumber = adjMatrix.Matrix[i, j];
+                        int altEdgeNumber = adjMatrix.Matrix[j,i];
+
+                        if (edgeNumber > 0 && edgeNumber!=altEdgeNumber)
+                        {
+                            incedenceMatrix[i, edgeNumber - 1] = -1;
+                            incedenceMatrix[j, edgeNumber -1] = 1;
+                        }
+
+                        if(altEdgeNumber > 0 && edgeNumber!=altEdgeNumber)
+                        {
+                            incedenceMatrix[i, edgeNumber - 1] = 1;
+                            incedenceMatrix[j, edgeNumber - 1] = -1;
+                        }
+
+                        if(altEdgeNumber>0 && edgeNumber == altEdgeNumber)
+                        {
+                            incedenceMatrix[i, edgeNumber - 1] = 1;
+                            incedenceMatrix[j, edgeNumber - 1] = 1;
+                        }
+
+                    }
+                }
+            }
+
+            return incedenceMatrix;
+        }
+
+        private int[,] FromAdjacencyList(AdjacencyList adjList)
+        {
+            throw new NotImplementedException();
+        }
+        #region Methods
+        #endregion
     }
 }
