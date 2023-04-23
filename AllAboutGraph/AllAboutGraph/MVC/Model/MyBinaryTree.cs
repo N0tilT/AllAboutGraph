cd264297d69b352ac773d3cf063983eb453d8784
N0tilT <timtompson3241@gmail.com>
2023-04-23 23:13:03 +0300

    BinaryTree

---
+++ AllAboutGraph/AllAboutGraph/MVC/Model/MyBinaryTree.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllAboutGraph.MVC.Model
+{
+    public class BinaryNode<T>
+    {
+        public T Data { get; set; }
+        public BinaryNode<T> Left { get; set; }
+        public BinaryNode<T> Right { get; set; }
+        public BinaryNode<T> Parent { get; set; }
+
+        public BinaryNode()
+        {
+        }
+
+        public BinaryNode(T data)
+        {
+            Data = data;
+        }
+
+        public BinaryNode(T data, BinaryNode<T> parent) : this(data)
+        {
+            Parent = parent;
+        }
+
+        public BinaryNode(T data, BinaryNode<T> parent, BinaryNode<T> left) : this(data, parent)
+        {
+            Left = left;
+        }
+
+        public BinaryNode(T data, BinaryNode<T> parent, BinaryNode<T> left, BinaryNode<T> right) : this(data, parent, left)
+        {
+            Right = right;
+        }
+
+        public override string ToString()
+        {
+            string node = "";
+            node += Data + " " + "\n" + Left?.ToString() + " - " + Right?.ToString();
+            return node;
+        }
+
+
+
+    }
+    public class MyBinaryTree<T> where T:IComparable<T>
+    {
+        public BinaryNode<T> Root { get; set; }
+        public int Count { get; set; }
+
+        public MyBinaryTree()
+        {
+            Count = 0;
+        }
+
+        public void Add(BinaryNode<T> item)
+        {
+            Count++;
+
+        }
+
+        public BinaryNode<T> Find(BinaryNode<T> root,T item)
+        {
+            bool flag = false;
+            if(root != null)
+            {
+                BinaryNode<T> currentNode = root;
+                if(currentNode.Data.Equals(item))
+                {
+                    flag = true;
+                    return currentNode;
+                }
+                else
+                {
+                    if(currentNode.Data.CompareTo(item) > 0)
+                    {
+                        return Find(currentNode.Left,item);
+                    }
+                    else
+                    {
+                        return Find(currentNode.Right, item);
+                    }
+                }
+            }
+            return null;
+        }
+
+        public BinaryNode<T> Min(BinaryNode<T> root)
+        {
+            BinaryNode<T> currentNode = root;
+            while(currentNode.Left != null) 
+            {
+                currentNode = currentNode.Left;
+            }
+            return currentNode;
+        }
+
+        public BinaryNode<T> Max(BinaryNode<T> root)
+        {
+            BinaryNode<T> currentNode = root;
+            while (currentNode.Right != null)
+            {
+                currentNode = currentNode.Right;
+            }
+            return currentNode;
+        }
+
+    }
+}
--- AllAboutGraph/AllAboutGraph/MVC/Model/TreeAlgorithms.cs
+++ AllAboutGraph/AllAboutGraph/MVC/Model/TreeAlgorithms.cs
@@ -10,7 +10,14 @@ namespace AllAboutGraph.MVC.Model
     {
         MyTree<T> Tree { get; set; }
 
-        public TreeAlgorithms(MyTree<T> tree) { Tree = tree; }
+        MyBinaryTree<T> BinaryTree { get; set; }
+
+        public TreeAlgorithms(MyTree<T> tree, MyBinaryTree<T> binaryTree)
+        {
+            Tree = tree; 
+            BinaryTree = binaryTree;
+        }
+
         public List<Node<T>> PreOrder(Node<T> root)
         {
             List<Node<T>> order = new List<Node<T>>();
@@ -54,5 +61,54 @@ namespace AllAboutGraph.MVC.Model
             }
             return order;
         }
+
+        public List<BinaryNode<T>> BinaryPreOrder(BinaryNode<T> root)
+        {
+            List<BinaryNode<T>> order = new List<BinaryNode<T>>();
+            if (root != null)
+            {
+                order.Add(root);
+
+                if(root.Left != null)
+                {
+                    order.AddRange(BinaryPreOrder(root.Left));
+                }
+                
+                if(root.Right != null)
+                {
+                    order.AddRange(BinaryPreOrder(root.Right));
+                }
+                
+            }
+            return order;
+        }
+
+        public List<BinaryNode<T>> BinaryPreOrder_NotRecursive(BinaryNode<T> root)
+        {
+            List<BinaryNode<T>> order = new List<BinaryNode<T>>();
+            
+            Stack<BinaryNode<T>> stack = new Stack<BinaryNode<T>>();
+
+            BinaryNode<T> currentNode = root;
+            while (true)
+            {
+                if(currentNode != null)
+                {
+                    order.Add(currentNode);
+                    stack.Push(currentNode);
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    if(stack.Count == 0)
+                    {
+                        return order;
+                    }
+                    stack.Pop();
+                    currentNode = currentNode.Right;
+                }
+            }
+        }
+
     }
 }
